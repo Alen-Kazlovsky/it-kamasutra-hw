24ca63c4ba1a8fe5a4fbc6ef545b2415d839b0e0
Alen Kazlovsky <alen-borisov@list.ru>
2022-10-30 17:43:42 +0300

    sd

--- src/application/settings.ts
+++ src/application/settings.ts
@@ -1,4 +1,4 @@
 export const settings = {
-    MONGO_URI: process.env.MongoURI || "mongodb+srv://alen-borisov:6712272OOOa@cluster0.qduucnh.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET: process.env.JWT_SECRET || "6712272OOOa"
+    MONGO_URI: process.env.MongoURI || "mongodb+srv://solikamsk:solikamsk@cluster0.uu9g6jj.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET: process.env.JWT_SECRET || "123"
 }
